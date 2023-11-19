2d4c83cc7e32a6475f518f657f4b4bf7a9c152c0
XXPegasXX <emivanov@inbox.ru>
2023-11-19 22:10:51 +0500

    added eslint (not working)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,21 @@
-import React, {useContext, useState} from 'react';
-import { Suspense } from 'react';
-import './styles/index.scss';
-import {Link, Route, Routes} from "react-router-dom";
-import AboutPage from "../pages/AboutPage/ui/AboutPage";
-import MainPage from "../pages/MainPage/ui/MainPage";
-import {Theme, ThemeContext} from "./providers/ThemeProvider/lib/ThemeContext";
-import {classNames} from "shared/lib/classNames/classNames";
-import { useTheme } from './providers/ThemeProvider';
-import AppRouter from "app/providers/router/ui/AppRouter";
-import {Navbar} from "widgets/Navbar";
-import { Sidebar } from 'widgets/Sidebar';
-import { useTranslation } from 'react-i18next';
+import React, { useContext, useState, Suspense } from 'react'
 
-
+import './styles/index.scss'
+import { Link, Route, Routes } from 'react-router-dom'
+import AboutPage from '../pages/AboutPage/ui/AboutPage'
+import MainPage from '../pages/MainPage/ui/MainPage'
+import { Theme, ThemeContext } from './providers/ThemeProvider/lib/ThemeContext'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { useTheme } from './providers/ThemeProvider'
+import AppRouter from 'app/providers/router/ui/AppRouter'
+import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
+import { useTranslation } from 'react-i18next'
 
 const App = () => {
-    const {theme} = useTheme();
+  const { theme } = useTheme()
 
-    return (
+  return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar/>
@@ -27,9 +25,9 @@ const App = () => {
                 </div>
             </Suspense>
 
-            {/*<button onClick={toggleTheme }>TOGGLE</button>*/}
+            {/* <button onClick={toggleTheme }>TOGGLE</button> */}
         </div>
-    );
-};
+  )
+}
 
-export default App;
+export default App
