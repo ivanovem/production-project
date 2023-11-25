292034a1990cdead4aaa6b2fbb0d6cd74d740538
XXPegasXX <emivanov@inbox.ru>
2023-11-25 17:16:51 +0500

    added PageLoader, Loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,22 +3,24 @@ import {Route, Routes} from "react-router-dom";
 import AboutPage from "pages/AboutPage/ui/AboutPage";
 import MainPage from "pages/MainPage/ui/MainPage";
 import {routeConfig} from "shared/config/routeConfig/routeConfig";
+import {PageLoader} from "shared/ui/PageLoader/PageLoader";
 
 const AppRouter = () => {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+
             <Routes>
                 {Object.values(routeConfig).map(({element, path}) => (
                     <Route
                         key={path}
                         element={(
-                            <div className='page-wrapper'>{element}</div>
+                            <Suspense fallback={<PageLoader/>}>
+                                <div className='page-wrapper'>{element}</div>
+                            </Suspense>
                         )}
                         path={path}
                     />
                 ))}
             </Routes>
-        </Suspense>
     );
 };
 
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,21 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import './Loader.scss';
+
+
+interface LoaderProps {
+    className?: string;
+}
+
+export const Loader = ({className}: LoaderProps) => {
+    return (
+        <div className={classNames('lds-ellipsis', {}, [className])}>
+            <div className="lds-ellipsis">
+                <div/>
+                <div/>
+                <div/>
+                <div/>
+            </div>
+        </div>
+    );
+};
+
