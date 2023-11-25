a7ec6eab2644273c4f66e2c7efc8090d4aab235c
XXPegasXX <emivanov@inbox.ru>
2023-11-25 17:23:08 +0500

    added PageLoader, Loader

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -2,3 +2,8 @@ import {lazy} from "react";
 
 // @ts-ignore
 export const AboutPageAsync = lazy(() => import('./AboutPage'));
+//
+// export const AboutPageAsync = lazy(() => new Promise((resolve) =>{
+//     //@ts-ignore
+//     setTimeout(()=> resolve(import('./AboutPage')), 1500);
+// }) );
--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -2,3 +2,7 @@ import {lazy} from "react";
 
 // @ts-ignore
 export const MainPageAsync = lazy(() => import('./MainPage'));
+// export const MainPageAsync = lazy(() => new Promise((resolve) =>{
+//     //@ts-ignore
+//     setTimeout(()=> resolve(import('./MainPage')), 1500);
+// }) );
