2ab71badaf21184f9313e27d48531538eeadd62e
XXPegasXX <emivanov@inbox.ru>
2023-12-21 18:01:46 +0500

    added storibook for Input and LoginForm

--- src/features/AuthByUserName/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUserName/ui/LoginForm/LoginForm.stories.tsx
@@ -1,15 +1,13 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type {Meta, StoryObj} from '@storybook/react'
 import React from 'react'
 
 import 'app/styles/index.scss'
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
-import { Theme } from 'app/providers/ThemeProvider'
-import {Input} from "shared/ui/Input/Input";
+import {LoginForm} from "./LoginForm";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-    title: 'shared/Input',
-    component: Input,
+    title: 'features/LoginForm',
+    component: LoginForm,
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
         layout: 'centered'
@@ -20,7 +18,8 @@ const meta = {
     argTypes: {
         backgroundColor: { control: 'color' }
     }
-} satisfies Meta<typeof Input>
+} satisfies Meta<typeof LoginForm>
+
 
 export default meta
 type Story = StoryObj<typeof meta>
@@ -28,7 +27,6 @@ type Story = StoryObj<typeof meta>
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
     args: {
-        placeholder: 'Type text',
-        value: '123123'
+
     }
 }
